98dab67778d73bc53a4d166d8265b92625c09316
ashandme <santiagosoleto121@gmail.com>
2026-09-10 19:15:38 -0300

    cambio de dir

---
+++ TGC.MonoGame.TP/Map.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TGC.MonoGame.TP
+{
+    internal class Map
+    {
+        private readonly List<Prop> _props = new();
+
+        public void LoadContent(ContentManager content)
+        {
+            // 1. Cargas los shaders y modelos UNA sola vez
+            var shader = content.Load<Effect>(TGCGame.ContentFolderEffects + "BasicShader");
+            var treeModel = content.Load<Model>(TGCGame.ContentFolder3D + "kenney_retro-urban-kit/");
+            var rockModel = content.Load<Model>(TGCGame.ContentFolder3D + "roca/roca");
+
+            // 2. Instancias los props reutilizando el mismo modelo (Instanciación lógica)
+            _props.Add(new Prop(treeModel, shader, new Vector3(0, 0, 10)));
+            _props.Add(new Prop(treeModel, shader, new Vector3(25, 0, 15), scale: new Vector3(1.5f)));
+            _props.Add(new Prop(rockModel, shader, new Vector3(-10, 0, 5)));
+        }
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+            // Podés iterar y dibujar todo el entorno
+            foreach (var prop in _props)
+            {
+                // Si cada prop tiene color propio o compartís uno genérico:
+                prop.Draw(view, projection, Color.DarkSlateGray.ToVector3());
+            }
+        }
+    }
+}
---
+++ TGC.MonoGame.TP/Prop.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TGC.MonoGame.TP;
+// CLASE PARA MANEJAR LOS MODELOS QUE IMPORTAMOS
+internal class Prop
+{
+    public Model Model { get; }
+    public Effect Effect { get; }
+    public Vector3 Position { get; set; }
+    public Vector3 Rotation { get; set; }
+    public Vector3 Scale { get; set; } = Vector3.One;
+    private readonly Matrix[] _boneTransforms;
+
+    public Prop(Model model, Effect effect, Vector3 position, Vector3? rotation = null, Vector3? scale = null)
+    {
+        Model = model;
+        Effect = effect;
+        Position = position;
+        Rotation = rotation ?? Vector3.Zero;
+        Scale = scale ?? Vector3.One;
+        foreach (var mesh in Model.Meshes)
+        {
+            foreach (var part in mesh.MeshParts)
+            {
+                part.Effect = Effect;
+            }
+        }
+        _boneTransforms = new Matrix[Model.Bones.Count];
+        Model.CopyAbsoluteBoneTransformsTo(_boneTransforms);
+    }
+    public Matrix GetWorldMatrix()
+    {
+        return Matrix.CreateScale(Scale) *
+               Matrix.CreateFromYawPitchRoll(Rotation.Y, Rotation.X, Rotation.Z) *
+               Matrix.CreateTranslation(Position);
+    }
+    public void Draw(Matrix view, Matrix projection, Vector3 diffuseColor)
+    {
+        var world = GetWorldMatrix();
+        // ESTO PUEDE CAMBIAR
+        Effect.Parameters["View"]?.SetValue(view);
+        Effect.Parameters["Projection"]?.SetValue(projection);
+        Effect.Parameters["DiffuseColor"]?.SetValue(diffuseColor);
+
+        foreach (var mesh in Model.Meshes)
+        {
+            var boneTransform = _boneTransforms[mesh.ParentBone.Index];
+            Effect.Parameters["World"]?.SetValue(boneTransform * world);
+            mesh.Draw();
+        }
+    }
+}
