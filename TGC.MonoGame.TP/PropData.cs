8289a9d672c26c4921daeca43e338cb10c288153
ashandme <santiagosoleto121@gmail.com>
2026-09-13 17:27:43 -0300

    un ; de mas

--- TGC.MonoGame.TP/PropData.cs
+++ TGC.MonoGame.TP/PropData.cs
@@ -18,5 +18,5 @@ public class PropData
     public Vector3 Position { get; set; } = Vector3.Zero;
     public Vector3 Rotation { get; set; } = Vector3.Zero;
     public Vector3 Scale { get; set; } = Vector3.One;
-    public Vector3? Color { get; set; };
+    public Vector3? Color { get; set; }
 }
